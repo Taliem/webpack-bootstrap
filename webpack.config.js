7371fb6fa9cd27f9b1e3eba82a0539c381d03c8f
Chingiz Golyshev <taliemjob@yandex.ru>
2018-12-17 13:57:41 +0700

    настроил базовую работу с css, html, изображениями, javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,56 @@
-// const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const devMode = process.env.NODE_ENV === "development";
+const isDevMode = process.env.NODE_ENV === "development";
 
 module.exports = {
 	entry: { 
 		index: "./src/index.js"
+	},
+	output: {
+		filename: "js/[name].js"
 	},
 	module: {
 		rules: [
 			{
 				test: /\.scss$/,
 				use: [
-					devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+					isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
 					"css-loader",
 					"sass-loader"
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+			    test: /\.(png|svg|jpg|gif)$/,
+			    use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'img/[name].[ext]'
+						}
+				  	}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'fonts/[[name].[ext]',
+						}
+				  }
 				]
 			}
 		]
@@ -24,9 +59,18 @@ module.exports = {
 		port: 8080
 	},
 	plugins: [
-		new MiniCssExtractPlugin(),
+		new CleanWebpackPlugin(["dist"]),
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css",
+		}),
 		new HtmlWebpackPlugin({
-			template: "./src/index.html"
-		})
+			template: 'src/index.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			filename: "about.html"
+		}),
+
+
 	]
 }
